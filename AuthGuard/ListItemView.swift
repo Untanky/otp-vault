7fc162f2b384abbcfabfb5636c4b8100a1bef48d
Lukas Grimm <lukaskingsmail@gmail.com>
2025-07-19 13:41:20 +0200

    Add animation showing value was copied

--- AuthGuard/ListItemView.swift
+++ AuthGuard/ListItemView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ListItemView: View {
+    @State private var showingCopied = false
+    @State private var isAnimating = false
+    
     private static let codeFont = Font.system(size: 24, weight: .bold, design: .monospaced)
     
     let oneTimePassword: OneTimePassword
@@ -34,10 +37,41 @@ struct ListItemView: View {
                     .foregroundStyle(.secondary)
             }
             Spacer()
-            Button(action: { onClickCode(String(code)) }) {
-                Text(code)
-                    .font(ListItemView.codeFont)
+            Button(action: {
+                guard !isAnimating else { return }
+                isAnimating = true
+                withAnimation(.spring()) {
+                    showingCopied = true
+                }
+                // Hide "COPIED" after 2 seconds
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation(.spring()) {
+                        showingCopied = false
+                    }
+                    // Allow another animation after transition completes
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+                        isAnimating = false
+                    }
+                }
+                
+                onClickCode(code)
+            }) {
+                ZStack {
+                    Text("COPIED")
+                        .font(ListItemView.codeFont)
+                        .foregroundColor(.green)
+                        .opacity(showingCopied ? 1 : 0)
+                        .offset(y: showingCopied ? 0 :45)
+                        .id("copied")
+                    Text(code)
+                        .font(ListItemView.codeFont)
+                        .foregroundColor(.blue)
+                        .opacity(showingCopied ? 0 : 1)
+                        .offset(y: showingCopied ? -45 : 0)
+                        .id("code")
+                }
             }
+            .animation(.spring(), value: showingCopied)
         }
         .onAppear {
             self.timerId = TimerService.shared.register(forInterval: oneTimePassword.period, callback: {
