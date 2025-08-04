8ed88746468150c58230febe7133d35cb763bbb0
Lukas Grimm <lukaskingsmail@gmail.com>
2025-08-04 12:51:41 +0200

    Adjust scanner view, adding text

--- AuthGuard/ScannerView.swift
+++ AuthGuard/ScannerView.swift
@@ -10,29 +10,59 @@ import SwiftUI
 
 struct ScannerView: View {
     let scannedOneTimePassword: (OneTimePassword) -> Void
+    @State private var error: Error?
     
     var body: some View {
-        CodeScannerView(
-            codeTypes: [.qr],
-            showViewfinder: true,
-            completion: handleScanResult
-        )
-        .ignoresSafeArea(edges: .bottom)
+        ZStack {
+            CodeScannerView(
+                codeTypes: [.qr],
+                showViewfinder: true,
+                completion: handleScanResult
+            )
+            .ignoresSafeArea(edges: .bottom)
+            
+            VStack {
+                Text("Scan QR code")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 8)
+                    .background(Material.regular, in: RoundedRectangle(cornerRadius: 12))
+                    .padding(.top, 60)
+                
+                if let error = error {
+                    HStack(alignment: .top) {
+                        Image(systemName: "exclamationmark.triangle.fill")
+                            .foregroundStyle(.red)
+                        Text(error.localizedDescription)
+                            .foregroundColor(.red)
+                    }
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 8)
+                    .background(Material.regular, in: RoundedRectangle(cornerRadius: 12))
+                    .frame(maxWidth: 300)
+                    
+                }
+                
+                Spacer()
+            }
+            .ignoresSafeArea(edges: .bottom)
+        }
     }
     
     private func handleScanResult(_ scanResult: Result<ScanResult, ScanError>) {
         switch scanResult {
         case .failure(let error):
-            print("Error: \(error)")
+            self.error = error
         case .success(let result):
             guard let uri = URL(string: result.string) else {
-                print("Error: could not parse url")
+                self.error = NSError(domain: "Invalid URI", code: 0, userInfo: nil)
                 return
             }
             let otpResult = OneTimePassword.parseUri(uri)
             switch otpResult {
             case .failure(let error):
-                print("Error: \(error)")
+                self.error = error
             case .success(let item):
                 scannedOneTimePassword(item)
             }
